dc533a8042644cbbd549ee94bb00dce3cb862419
seowfie <144519653+seowfie@users.noreply.github.com>
2025-10-08 00:52:56 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/HomePage.vue'
 import Garden from '../pages/GardenPage.vue'
+import Login from '@/pages/LogIn.vue'
+import Signup from '@/pages/SignUp.vue'
+import Profile from '@/pages/ProfileUser.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Home },
+    { path: '/login', component: Login },
+    { path: '/signup', component: Signup },
     { path: '/garden', component: Garden },
+    { path: '/profile', component: Profile },
   ],
 })
 
