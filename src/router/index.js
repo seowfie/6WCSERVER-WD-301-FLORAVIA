5b0060c610bf39999688ee8214844735848a81aa
Korli <adriancurley9@gmail.com>
2025-10-07 17:20:59 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
-import Garden from '../pages/Garden.vue'
+import Home from '../pages/HomePage.vue'
+import Garden from '../pages/GardenPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
