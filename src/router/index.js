cf66627475697219fdbeabd98c444ea48fb42e38
Korli <adriancurley9@gmail.com>
2025-10-09 03:09:58 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/HomePage.vue'
 import Garden from '../pages/GardenPage.vue'
-import Login from '@/pages/LogIn.vue'
-import Signup from '@/pages/SignUp.vue'
-import Profile from '@/pages/ProfileUser.vue'
+import Login from '../pages/LogIn.vue'
+import Signup from '../pages/SignUp.vue'
+import Profile from '../pages/ProfileUser.vue'
+import SingleProduct from '../pages/SingleProduct.vue'
+import CheckOut from '../pages/CheckOut.vue'
+import Cart from '../pages/CartPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Home },
+    { path: '/garden', component: Garden },
     { path: '/login', component: Login },
     { path: '/signup', component: Signup },
-    { path: '/garden', component: Garden },
     { path: '/profile', component: Profile },
+    { path: '/singleproduct', component: SingleProduct },
+    { path: '/checkout', component: CheckOut },
+    { path: '/cart', component: Cart },
   ],
 })
 
