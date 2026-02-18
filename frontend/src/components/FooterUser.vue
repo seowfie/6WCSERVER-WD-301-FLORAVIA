5df8c48ab1ab1bef0e88e91faacc865ca64fe2e2
seowfie <144519653+seowfie@users.noreply.github.com>
2026-02-19 00:01:58 +0800

    Fix capitalization of TikTok icon import

--- frontend/src/components/FooterUser.vue
+++ frontend/src/components/FooterUser.vue
@@ -72,7 +72,7 @@ import emailIcon from '@/assets/vectors/Email.png'
 // Social vectors
 import facebookIcon from '@/assets/vectors/Facebook.png'
 import instagramIcon from '@/assets/vectors/Instagram.png'
-import tiktokIcon from '@/assets/vectors/Tiktok.png'
+import tiktokIcon from '@/assets/vectors/TikTok.png'
 import emailconIcon from '@/assets/vectors/Email_Connect.png'
 </script>
 
